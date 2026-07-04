4a8224ac5e173f041d53271c3d76865b4de32c14
SiddhikShaikh-Dev <siddhikshaikh955@gmail.com>
2026-07-04 19:27:10 +0000

    Loading Animation

--- script.js
+++ script.js
@@ -53,3 +53,12 @@ function typeEffect() {
 }
 
 typeEffect();
+window.addEventListener("load", function () {
+
+    setTimeout(function () {
+
+        document.getElementById("loader").style.display = "none";
+
+    }, 1000);
+
+});
