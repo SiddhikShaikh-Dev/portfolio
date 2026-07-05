d1bbcae2f17a85e4d5efaefb5964031c253f1dab
SiddhikShaikh-Dev <siddhikshaikh955@gmail.com>
2026-07-05 09:08:48 +0000

    Updated

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
-const text = "Aspiring Full Stack Web Devloper";
+const text = "Aspiring Full Stack Developer";
 let index = 0;
 
 function typeEffect() {
